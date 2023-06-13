78d517061e88954f6240becb0d984b83e30e99e3
rinarudhei <rinaldiadrian5@gmail.com>
2023-06-14 00:04:32 +0700

    yippi

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,24 @@
+type BinaryNode<T> = {
+    value: number,
+    left: BinaryNode<T>|null,
+    right: BinaryNode<T>|null,
+}
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length) {
+        const next = q.shift() as BinaryNode<number>;
+
+        if (next.value === needle) {
+            return true;
+        }
+        if (next.left) {
+            q.push(next.left);
+        }
+        if (next.right) {
+            q.push(next.right);
+        }
+    }
+
+    return false;
 }
