09fa7a1d0b73ee02c44b69080217c8af23506893
rinarudhei <rinaldiadrian5@gmail.com>
2023-06-14 21:15:32 +0700

    compare binary trees done

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,21 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+type BinaryNode<T> = {
+    value: number,
+    left: BinaryNode<T>|null,
+    right: BinaryNode<T>|null,
+}
 
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a === null && b === null) {
+        return true;
+    }
+
+    if (a === null || b === null) {
+        return false;
+    }
+
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
